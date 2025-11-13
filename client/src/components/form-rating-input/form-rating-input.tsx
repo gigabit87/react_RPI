d8fa24291105e3166a631555cd6d3449e2306c8a
alxksxy <alxksxy17@gmail.com>
2025-11-14 02:02:17 +0300

    1

--- client/src/components/form-rating-input/form-rating-input.tsx
+++ client/src/components/form-rating-input/form-rating-input.tsx
@@ -2,9 +2,13 @@ import { JSX } from "react";
 
 function FormRatingInput(): JSX.Element{
     return(
-        <li className="offer__inside-item">
-                    Wi-Fi
-                  </li>
+        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio">
+                    <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
+                      <svg className="form__star-image" width="37" height="33">
+                        <use href="#icon-star"></use>
+                      </svg>
+                    </label>
+        </input>
     )
 }
 
